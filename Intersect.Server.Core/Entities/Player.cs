645234fd289061c7ce26c54bb23e252e078c4aed
cydyn <144380768+cydyn@users.noreply.github.com>
2024-04-12 08:07:29 +0200

    Proper operation of projectile

--- Intersect.Server.Core/Entities/Player.cs
+++ Intersect.Server.Core/Entities/Player.cs
@@ -218,12 +218,6 @@ namespace Intersect.Server.Entities
         [NotMapped, JsonIgnore]
         public bool IsFading { get; set; }
 
-        /// <summary>
-        /// Reference stored of the last weapon used for an auto-attack
-        /// </summary>
-        [NotMapped, JsonIgnore]
-        public ItemBase LastAttackingWeapon { get; set; }
-
         // Instancing
         public MapInstanceType InstanceType { get; set; } = MapInstanceType.Overworld;
 
@@ -1491,7 +1485,6 @@ namespace Intersect.Server.Entities
         {
             if (isAutoAttack)
             {
-                EnqueueStartCommonEvent(LastAttackingWeapon?.GetEventTrigger(ItemEventTriggers.OnHit));
                 foreach (var trigger in CachedEquipmentOnHitTriggers)
                 {
                     EnqueueStartCommonEvent(trigger);
@@ -1605,8 +1598,6 @@ namespace Intersect.Server.Entities
                     }
                 }
             }
-
-            LastAttackingWeapon = weapon;
 
             if (weapon != null)
             {
@@ -5822,7 +5813,7 @@ namespace Intersect.Server.Entities
 
                 // We have special logic for handling weapons, so the player can't hot-swap their weapon and get a different on-hit event to proc
                 // As a result, don't cache them, instead use property "LastAttackingWeapon"
-                if (onHit != null && slot != Options.WeaponIndex)
+                if (onHit != null)
                 {
                     CachedEquipmentOnHitTriggers.Add(onHit);
                 }
